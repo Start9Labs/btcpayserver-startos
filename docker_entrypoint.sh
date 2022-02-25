88b86fa165228bade9ef2104d567b0f88ea409c6
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2022-02-25 13:00:44 -0700

    check for mountpoint existence on startup

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -12,6 +12,28 @@ _term() {
   kill -TERM "$btcpayserver_process" 2>/dev/null
 }
 
+if ! test -d /mnt/lnd then
+  echo "LND mountpoint does not exist"
+  exit 0
+fi
+
+if ! test -d /mnt/c-lightning then
+  echo "C-Lightning mountpoint does not exist"
+  exit 0
+fi
+
+while ! test -f /mnt/lnd/admin.macaroon
+do
+  echo "Waiting for LND admin macaroon to be generated..."
+  sleep 1
+done
+
+while ! test -S /mnt/c-lightning/shared/lightning-rpc
+do
+    echo "Waiting for bitcoin RPC socket..."
+    sleep 1
+done
+
 configurator > .env 
 source .env
 
