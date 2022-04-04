080d9c7533035a197bf7ccc22a6749982b454436
Keagan McClelland <keagan.mcclelland@gmail.com>
2022-04-04 14:28:56 -0600

    fix dyslexia in migration

--- migrations/to_lt_1_4_7.sh
+++ migrations/to_lt_1_4_7.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-yq e -i '.bitcoind["bitcoin-rpc"] = {"type":"internal","rpc-user":null,"rpc-password":null}' /datadir/start9/config.yaml
+yq e -i '.bitcoin["bitcoind-rpc"] = {"type":"internal","rpc-user":null,"rpc-password":null}' /datadir/start9/config.yaml
 echo -n '{"configured":false}'
