9dc44a94b36156e030e32aa51527ffdd4c28b7a3
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2022-08-17 19:29:58 -0600

    fix migrations to create folder when previous version was not configured

--- scripts/services/migrations.ts
+++ scripts/services/migrations.ts
@@ -2,28 +2,34 @@ import { types as T, compat } from "../deps.ts"
 import { migration_up_1_4_7 } from "../migrations/1_4_7_up_migration.ts";
 import { migration_down_1_4_7 } from "../migrations/1_4_7_down_migration.ts";
 
-export const migration: T.ExpectedExports.migration = compat.migrations
-  .fromMapping(
-    {
-      // 1.1.2.4: initial version released with eOS 0.3.0 - bitcoin config was internal (proxy) or external
-      "1.4.7": {
-        up: compat.migrations.updateConfig(
-          (config) => {
-            return migration_up_1_4_7(config)
-          },
-          false,
-          { version: "1.4.7", type: "up" },
-        ),
-        down: compat.migrations.updateConfig(
-          (config) => {
-            return migration_down_1_4_7(config)
-          },
-          false,
-          { version: "1.4.7", type: "down" },
-        ),
+export const migration: T.ExpectedExports.migration = async (effects, version, ...args) => {
+  await effects.createDir({
+    path: "start9",
+    volumeId: "main"
+  });
+  return compat.migrations
+    .fromMapping(
+      {
+        // 1.1.2.4: initial version released with eOS 0.3.0 - bitcoin config was internal (proxy) or external
+        "1.4.7": {
+          up: compat.migrations.updateConfig(
+            (config) => {
+              return migration_up_1_4_7(config)
+            },
+            false,
+            { version: "1.4.7", type: "up" },
+          ),
+          down: compat.migrations.updateConfig(
+            (config) => {
+              return migration_down_1_4_7(config)
+            },
+            false,
+            { version: "1.4.7", type: "down" },
+          ),
+        },
+        // 1.4.7.2: no migration needed
+        // 1.4.7.3: no migration needed - note: JS config/properties conversion occurred
       },
-      // 1.4.7.2: no migration needed
-      // 1.4.7.3: no migration needed - note: JS config/properties conversion occurred
-    },
-    "1.6.6",
-  );
+      "1.6.6",
+    )(effects, version, ...args)
+}
