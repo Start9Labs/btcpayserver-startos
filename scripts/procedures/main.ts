898fae5446e29887ad90d321c30d5e88a6ed4277
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2022-11-23 11:14:44 -0700

    user docker for main and health checks

--- scripts/procedures/main.ts
+++
@@ -1,6 +0,0 @@
-import { types as T } from "../deps.ts";
-
-export const main = (effects: T.Effects) => {
-  // args defaulted to [] - not necessary to include if empty
-  return effects.runDaemon({ command: "/init", args: [] }).wait()
-}
