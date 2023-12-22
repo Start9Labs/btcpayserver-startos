dd81f660ac9a43980eb00dd98c5a1ace4e948475
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2023-12-22 16:43:00 -0500

    update to 1.12.3

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -48,6 +48,6 @@ export const migration: T.ExpectedExports.migration = async (effects, version, .
           ),
         },
       },
-      "1.10.3.1",
+      "1.12.3",
     )(effects, version, ...args)
 }
