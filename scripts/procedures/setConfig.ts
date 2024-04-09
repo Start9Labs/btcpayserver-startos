042f8267f910f2a13ab3487d3155ec9b47cab722
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2024-04-09 17:47:18 -0400

    base setup for monero

--- scripts/procedures/setConfig.ts
+++ scripts/procedures/setConfig.ts
@@ -8,9 +8,15 @@ export const setConfig: T.ExpectedExports.setConfig = async (effects, input ) =>
 
   const depsLnd: T.DependsOn = newConfig?.lightning?.type === "lnd"  ? {lnd: []} : {}
   const depsCln: T.DependsOn = newConfig?.lightning?.type === "c-lightning"  ? {"c-lightning": []} : {}
+  const depsMonero: T.DependsOn = newConfig?.altcoins?.monero.enabled  ? {"monerod": []} : {}
+
+  if (newConfig?.altcoins?.monero.enabled) {
+    await effects.createDir({ volumeId: "main", path: "~/.bitmonero/wallets" })
+  }
 
   return await compat.setConfig(effects,input, {
     ...depsLnd,
     ...depsCln,
+    ...depsMonero
   })
 }
