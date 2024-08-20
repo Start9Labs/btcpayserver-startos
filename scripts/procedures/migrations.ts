edef432c517bd4aabd5864418089279a3fc3abcd
Lucy <12953208+elvece@users.noreply.github.com>
2024-08-20 17:57:26 -0400

    update to upstream 1.13.5 (#68)

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -48,6 +48,6 @@ export const migration: T.ExpectedExports.migration = async (effects, version, .
           ),
         },
       },
-      "1.13.3",
+      "1.13.5",
     )(effects, version, ...args)
 }
