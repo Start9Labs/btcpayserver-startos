0a6968a31dafe714d8bc5196d740b1469e6a5889
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2024-10-21 15:48:48 -0400

    update to upstream 2.0.0

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -48,6 +48,6 @@ export const migration: T.ExpectedExports.migration = async (effects, version, .
           ),
         },
       },
-      "1.13.5",
+      "2.0.0",
     )(effects, version, ...args)
 }
